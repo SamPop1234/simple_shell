921ab661dd34dced08460cce38d2eb9ae189444d
SamPop1234 <samuel.popoola@ust.edu.ng>
2023-11-08 00:01:06 -0800

    Atoi

--- Atoi.c
+++ Atoi.c
@@ -1,23 +1,22 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
+ * interact - returns true if shell is interactive mode
  * @info: struct address
- *
  * Return: 1 if interactive mode, 0 otherwise
  */
-int interactive(info_t *info)
+int interact(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delim - checks if character is a delimeter
+ * delim_check - checks if character is a delimeter
  * @c: the char to check
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int is_delim(char c, char *delim)
+int delim_check(char c, char *delim)
 {
 	while (*delim)
 		if (*delim++ == c)
@@ -26,40 +25,40 @@ int is_delim(char c, char *delim)
 }
 
 /**
- *_isalpha - checks for alphabetic character
+ *alpha_check - checks for alphabetic character
  *@c: The character to input
  *Return: 1 if c is alphabetic, 0 otherwise
  */
 
-int _isalpha(int c)
+int alpha_check(int i)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
 
 /**
- *_atoi - converts a string to an integer
+ *c_atoi - converts a string to an integer
  *@s: the string to be converted
  *Return: 0 if no numbers in string, converted number otherwise
  */
 
-int _atoi(char *s)
+int c_atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
+	int j, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	for (j = 0;  s[j] != '\0' && flag != 2; j++)
 	{
-		if (s[i] == '-')
+		if (s[j] == '-')
 			sign *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[j] >= '0' && s[j] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (s[i] - '0');
+			result += (s[j] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
