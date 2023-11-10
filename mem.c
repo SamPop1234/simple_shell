784ced13e2529cf7ba9d3e02e4baaaec9aba8560
Celo90 <agbahowecomfort@gmail.com>
2023-11-10 03:45:50 -0800

    mem.c

--- mem.c
+++ mem.c
@@ -2,9 +2,9 @@
 
 /**
  * mfree - frees a pointer and NULLs the address
- * @ptr: address of the pointer to free
+ * @ptr: pointer to be freed
  *
- * Return: 1 if freed, otherwise 0.
+ * Return: 1 if successfully freed, else 0.
  */
 int mfree(void **ptr)
 {
