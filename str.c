ac7491f50221508d0d7e4a54e6e973d35dfe447c
Celo90 <agbahowecomfort@gmail.com>
2023-11-10 13:28:41 -0800

    str.c

--- str.c
+++ str.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * _strlen - the length of a string that should be returned
+ * @s: the string length to check
  *
- * Return: integer length of string
+ * Return: integer
  */
 int _strlen(char *s)
 {
@@ -19,9 +19,9 @@ int _strlen(char *s)
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
+ * _strcmp - comparison of two strings.
+ * @s1: string one
+ * @s2: string two
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
@@ -42,7 +42,7 @@ int _strcmp(char *s1, char *s2)
 
 /**
  * starts_with - checks if needle starts with haystack
- * @haystack: string to search
+ * @haystack: search string
  * @needle: the substring to find
  *
  * Return: address of next char of haystack or NULL
@@ -57,10 +57,10 @@ char *starts_with(const char *haystack, const char *needle)
 
 /**
  * _strcat - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
+ * @dest: destination buffer
+ * @src: source buffer
  *
- * Return: pointer to destination buffer
+ * Return: pointer to buffer
  */
 char *_strcat(char *dest, char *src)
 {
