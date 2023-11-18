b69d851baf0985ffcf999b2eb0a491d5ac256893
Celo90 <agbahowecomfort@gmail.com>
2023-11-17 17:00:40 -0800

    string.c

--- string.c
+++ string.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * _strlen - the length of a string to be returned
+ * @s: string len to be checked
  *
- * Return: integer length of string
+ * Return: length of string (integer)
  */
 int _strlen(char *s)
 {
@@ -19,9 +19,9 @@ int _strlen(char *s)
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
+ * _strcmp - create lexicogarphic comparison of two str.
+ * @s1: first str
+ * @s2: second str
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
@@ -41,11 +41,11 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * starts_with - checks if needle starts with haystack
- * @haystack: string to search
- * @needle: the substring to find
+ * starts_with - check wethe rneedle starts with haystack
+ * @haystack: to search string
+ * @needle: find substring
  *
- * Return: address of next char of haystack or NULL
+ * Return: next char address of haystack or return NULL
  */
 char *starts_with(const char *haystack, const char *needle)
 {
@@ -56,11 +56,11 @@ char *starts_with(const char *haystack, const char *needle)
 }
 
 /**
- * _strcat - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
+ * _strcat - two strings to be concatenated
+ * @dest: the buff dest
+ * @src: the buff source
  *
- * Return: pointer to destination buffer
+ * Return: destination buff pointer
  */
 char *_strcat(char *dest, char *src)
 {
